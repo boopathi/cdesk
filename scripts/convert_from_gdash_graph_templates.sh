c9d85a921a1bb41d187b68f08594b864f1eec316
Boopathi Rajaa <me@boopathi.in>
2012-10-09 21:49:19 +0530

    convert script

--- scripts/convert_from_gdash_graph_templates.sh
+++ scripts/convert_from_gdash_graph_templates.sh
@@ -3,13 +3,39 @@
 GDASH_LOCATION=/var/www/html/gdash
 GRAPH_TEMPLATES=$GDASH_LOCATION/graph_templates
 
-RAND=`cat /dev/urandom | head -c ${1:-32} | tr -cd "[:alnum:]"`
+#app settings
+TABLENAME=dashboard_graph
 
-find $GRAPH_TEMPLATES -name "*.graph" | xargs cat $1 | grep ":data*\$HOST" | cut -d "," -f2 | cut -d ')' -f1 > /tmp/graph_data-$RAND
+SCRIPTNAME=$0
+DBLOCATION=$1
+
+#obtain these values from the sqlite3 database in $TABLENAME
+http=1
+ftp=2
+dns=3
+mail=4
+mysql=5
+power=6
+system=7
+
+RAND=`cat /dev/urandom | head -c 10 | tr -cd "[:alnum:]"`
+
+function foo { echo $1; exit 1; }
+
+[ -f $GRAPH_TEMPLATES ] || foo "Graph templates not found"
+
+find $GRAPH_TEMPLATES -name "*.graph" | xargs cat $1 | grep ":data" | cut -d "," -f2 | cut -d ')' -f1 | grep "HOST" > /tmp/graph_data-$RAND
 
 cat /tmp/graph_data-$RAND | while read line;
 do
-    echo ${line//\$HOST//\%s}
+    data=${line/\$HOST/\%s}
+    service=`echo $data | cut -d '.' -f3`
+    label=`echo ${data##*.}`
+    sname=`eval echo $\`echo $service\``
+    Query="INSERT INTO $TABLENAME (sname_id,label,data) VALUES ($sname,'$label','$data');"
+    if [ -f $DBLOCATION ]; then
+	sqlite3 $DBLOCATION $Query
+    else
+	echo $Query
+    fi
 done
-
-
