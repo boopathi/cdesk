5b3c6c7d56d8a0f07fc505eb43f0f410618111b1
Boopathi Rajaa <me@boopathi.in>
2012-10-09 22:57:23 +0530

    Added loader

---
+++ static/cdesk.js
@@ -0,0 +1,17 @@
+$(function(){
+    var total = $("img").length;
+    var counter = 0;
+    var log = function() {
+	var percent = 100*++counter/total;
+	$("#imgloader").width(percent + "%").html(Math.ceil(percent) + "%");
+	if(percent==100){
+	    $("#imgloader").html(" 100%. Loading Complete :) ");
+	    setTimeout(function(){
+		$("#imgloader").parent(".progress").fadeOut(500);
+	    }, 1250);
+	}
+    }
+    $("img").bind({
+	"load": function(){ log(); }
+    });
+});
