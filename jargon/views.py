e33099b61eda995131a247e2dd0a27f2954740db
Boopathi Rajaa <me@boopathi.in>
2012-10-31 11:08:04 +0530

    Now supports import whisper module - Pushing it for testing

--- jargon/views.py
+++ jargon/views.py
@@ -4,12 +4,28 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
 
-def _whisper_fetch():
+def _whisper_fetch(path):
     #imported from whisperfetch.py
-    return false
+    import sys, time
+    import whisper
+    basepath = "/data/graphite/whisper/"
+    path = basepath+str(path)
+    from_time='0'
+    until_time='now'
+    (timeInfo, values) = whisper.fetch(path, from_time, until_time)
+    (start,end,step)=timeInfo
+    values_json = str(values).replace('None','null')
+    return '''{
+"start": %d,
+"end": $d,
+"step": %d,
+"values": %s
+}''' % (start, end, step, values_json)
 
 def index(request):
     context = {
         'servers': [1,2,3,4]
     }
+    jsondata = _whisper_fetch('hosting/cp-23/mysql/modsec/total_dbs_size.wsp')
+    context['data'] = jsondata
     return render_to_response('jargon.html', context)
