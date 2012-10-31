78c70fdce36ccf79048dea73074427b2bbec9517
Boopathi Rajaa <me@boopathi.in>
2012-10-31 22:15:04 +0530

    CSRF

--- cdesk/settings.py
+++ cdesk/settings.py
@@ -20,6 +20,10 @@ DATABASES = {
     }
 }
 
+#CSRF settings
+CSRF_COOKIE_NAME = 'graphite#5'
+CSRF_COOKIE_DOMAIN = 'graphite.directi.com'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
