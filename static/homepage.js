e4a8f4ece20f6e3fee3030d39870a3d493120efa
Boopathi Rajaa <me@boopathi.in>
2012-11-02 19:41:55 +0530

    CSS

--- static/homepage.js
+++ static/homepage.js
@@ -1,15 +1,15 @@
 $(function() {
     $(".icon").hover(function() {
 	$(this).stop().animate({
-	    width: 360,
-	    height: 360,
+	    width: 320,
+	    height: 320,
 	    borderWidth: 0
 	},100);
     },function() {
 	$(this).stop().animate({
 	    width: 300,
 	    height: 300,
-	    borderWidth: 30
+	    borderWidth: 10
 	}, 250);
     });
 });
