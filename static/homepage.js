ada1e35fb21bc7690a21fcf2ce515925e78d3c05
Boopathi Rajaa <me@boopathi.in>
2012-11-02 19:34:18 +0530

    TEST

--- static/homepage.js
+++ static/homepage.js
@@ -2,7 +2,8 @@ $(function() {
     $(".icon").hover(function() {
 	$(this).stop().animate({
 	    width: 350,
-	    height: 350
+	    height: 350,
+	    border: 5
 	},100);
     },function() {
 	$(this).stop().animate({
