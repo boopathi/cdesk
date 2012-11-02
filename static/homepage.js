ec038419b0abe06e4c2cf59ad8b59142e245c545
Boopathi Rajaa <me@boopathi.in>
2012-11-02 20:25:59 +0530

    Working_Fine7

--- static/homepage.js
+++ static/homepage.js
@@ -3,13 +3,15 @@ $(function() {
 	$(this).stop().animate({
 	    width: 310,
 	    height: 310,
-	    borderWidth: 0
+	    borderWidth: 0,
+	    backgroundColor: "#EEE"
 	},100);
     },function() {
 	$(this).stop().animate({
 	    width: 300,
 	    height: 300,
-	    borderWidth: 5
+	    borderWidth: 5,
+	    backgroundColor: $(this).data("bg")
 	}, 250);
     });
 });
